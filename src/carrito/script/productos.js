4720c63e3a4006a63f8c1710e90fb52ccf17b239
alejandrohernandez1998 <alejandro.hernandezh@sansano.usm.cl>
2023-09-11 21:13:51 -0300

    carrito

--- src/carrito/script/productos.js
+++ src/carrito/script/productos.js
@@ -2,122 +2,53 @@
 const productos = [
     // Abrigos
     {
-        id: "abrigo-01",
-        titulo: "Abrigo 01",
-        imagen: "./img/abrigos/01.jpg",
-        precio: 1010
+        id: "sushi-01",
+        titulo: "Veggie Keto Oriental",
+        imagen: "../images/menu1.webp",
+        precio: 10000
     },
     {
-        id: "abrigo-02",
-        titulo: "Abrigo 02",
-        imagen: "./img/abrigos/02.jpg",
-        precio: 900
+        id: "sushi-02",
+        titulo: "Cheese Roll",
+        imagen: "../images/menu6.webp",
+        precio: 10000
     },
     {
-        id: "abrigo-03",
-        titulo: "Abrigo 03",
-        imagen: "./img/abrigos/03.jpg",
-        precio: 2000
+        id: "sushi-03",
+        titulo: "Samurai Roll",
+        imagen: "../images/menu8.webp",
+        precio: 10000
     },
     {
-        id: "abrigo-04",
-        titulo: "Abrigo 04",
-        imagen: "./img/abrigos/04.jpg",
-        precio: 1500
+        id: "sushi-04",
+        titulo: "Cheese Tempura",
+        imagen: "../images/menu9.webp",
+        precio: 10000
     },
     {
-        id: "abrigo-05",
-        titulo: "Abrigo 05",
-        imagen: "./img/abrigos/05.jpg",
-        precio: 600
-    },
-    // Camisetas
-    {
-        id: "camiseta-01",
-        titulo: "Camiseta 01",
-        imagen: "./img/camisetas/01.jpg",
-        precio: 1500
+        id: "sushi-05",
+        titulo: "Sashimi Tempura Atún",
+        imagen: "../images/menu10.webp",
+        precio: 10000
     },
     {
-        id: "camiseta-02",
-        titulo: "Camiseta 02",
-        imagen: "./img/camisetas/02.jpg",
-        precio: 2100
+        id: "sushi-06",
+        titulo: "Sashimi Atún",
+        imagen: "../images/menu11.webp",
+        precio: 10000
     },
     {
-        id: "camiseta-03",
-        titulo: "Camiseta 03",
-        imagen: "./img/camisetas/03.jpg",
-        precio: 2300
+        id: "sushi-07",
+        titulo: "Tropical Roll",
+        imagen: "../images/menu12.webp",
+        precio: 10000
     },
     {
-        id: "camiseta-04",
-        titulo: "Camiseta 04",
-        imagen: "./img/camisetas/04.jpg",
-
-        precio: 500
+        id: "sushi-08",
+        titulo: "Strawberry Roll",
+        imagen: "../images/menu13.webp",
+        precio: 10000
     },
-    {
-        id: "camiseta-05",
-        titulo: "Camiseta 05",
-        imagen: "./img/camisetas/05.jpg",
-        
-        precio: 100
-    },
-    {
-        id: "camiseta-06",
-        titulo: "Camiseta 06",
-        imagen: "./img/camisetas/06.jpg",
-        
-        precio: 100
-    },
-    {
-        id: "camiseta-07",
-        titulo: "Camiseta 07",
-        imagen: "./img/camisetas/07.jpg",
-        
-        precio: 500
-    },
-    {
-        id: "camiseta-08",
-        titulo: "Camiseta 08",
-        imagen: "./img/camisetas/08.jpg",
-        
-        precio: 500
-    },
-    // Pantalones
-    {
-        id: "pantalon-01",
-        titulo: "Pantalón 01",
-        imagen: "./img/pantalones/01.jpg",
-        
-        precio: 600
-    },
-    {
-        id: "pantalon-02",
-        titulo: "Pantalón 02",
-        imagen: "./img/pantalones/02.jpg",
-        
-        precio: 600
-    },
-    {
-        id: "pantalon-03",
-        titulo: "Pantalón 03",
-        imagen: "./img/pantalones/03.jpg",
-        precio: 700
-    },
-    {
-        id: "pantalon-04",
-        titulo: "Pantalón 04",
-        imagen: "./img/pantalones/04.jpg",
-        precio: 800
-    },
-    {
-        id: "pantalon-05",
-        titulo: "Pantalón 05",
-        imagen: "./img/pantalones/05.jpg",
-        precio: 1300
-    }
 ];
 
 
